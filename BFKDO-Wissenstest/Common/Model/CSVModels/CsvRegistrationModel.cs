bfcb63f2c56ecb9e530519c5db6d4285e4036e45
bennimoser <62222316+bennimoser@users.noreply.github.com>
2023-04-30 20:28:24 +0200

    Feature: CSV einlesen

--- BFKDO-Wissenstest/Common/Model/CSVModels/CsvRegistrationModel.cs
+++ BFKDO-Wissenstest/Common/Model/CSVModels/CsvRegistrationModel.cs
@@ -7,22 +7,40 @@ namespace Common.Model.CSVModels
     /// </summary>
     public class CsvRegistrationModel
     {
+        /// <summary>
+        ///     Nachname.
+        /// </summary>
         [Name("Nachname")]
         public string LastName { get; set; } = string.Empty;
 
+        /// <summary>
+        ///     Vorname.
+        /// </summary>
         [Name("Vorname")]
         public string FirstName { get; set; } = string.Empty;
 
-        [Name("Geburtsdatum")]
-        [Format("dd.MM.yyyy")]
-        public DateTime Birthday { get; set; }
+        /// <summary>
+        ///     Geburtsdatum.
+        /// </summary>
+        //[Name("Geburtsdatum")]
+        //[Format("dd.MM.yyyy")]
+        //public DateTime Birthday { get; set; }
 
+        /// <summary>
+        ///     Wertungsklasse.
+        /// </summary>
         [Name("Wertungsklasse")]
         public string RegistratedLevel { get; set; } = string.Empty;
 
+        /// <summary>
+        ///     Angemeldete Feuerwehr.
+        /// </summary>
         [Name("Feuerwehr")]
         public string Station { get; set; } = string.Empty;
 
+        /// <summary>
+        ///     Sybos Id.
+        /// </summary>
         [Name("ADadrnr")]
         public long SybosId { get; set; }
     }
