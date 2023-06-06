00a9e63cf2fabc0f7dced04ced0097d3fb45e324
bennimoser <62222316+bennimoser@users.noreply.github.com>
2023-06-06 18:45:00 +0200

    CORS Bug Fix

--- BFKDO-Wissenstest/WebAPI/Program.cs
+++ BFKDO-Wissenstest/WebAPI/Program.cs
@@ -29,7 +29,11 @@ namespace WebAPI
                 options.AddDefaultPolicy(
                     policy =>
                     {
+#if DEBUG
                         policy.WithOrigins("https://localhost:7022", "https://localhost:7227").AllowAnyHeader().AllowAnyMethod();
+#else
+                        policy.WithOrigins("https://bfkdo-adminapp.azurewebsites.net/", "https://bfkdo-benutzerapp.azurewebsites.net/").AllowAnyHeader().AllowAnyMethod();
+#endif
                     });
             });
 
