50ae02f56a540dc7a976804c2e568f934885f9e7
bennimoser <62222316+bennimoser@users.noreply.github.com>
2023-06-09 09:27:51 +0200

    feature: End To End Tests für Admin-App

---
+++ BFKDO-Wissenstest/EndToEndTests/BenutzerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndToEndTests
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class BenutzerTests
+    {
+        private string _url;
+
+        [SetUp]
+        public void Setup()
+        {
+            _url = "http://localhost:5194";
+        }
+
+
+    }
+}
